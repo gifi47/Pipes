1859b6cb37fd4dcbc191e4526d015c0ae866fd62
gifi47 <85646384+gifi47@users.noreply.github.com>
2024-05-06 17:39:24 +0300

    Add README.MD
    added readme.md

---
+++ Assets/Scripts/to_utf8.ps1
@@ -0,0 +1,18 @@
+﻿# Specify the directory containing the files
+$directory = $PSScriptRoot
+
+# Get all files in the directory
+$files = Get-ChildItem -Path $directory -Recurse -File | Where-Object { $_.Extension -ne '.meta' }
+
+# Loop through each file
+foreach ($file in $files) {
+    # Read the file content with the correct encoding
+    $content = Get-Content -Path $file.FullName -Raw
+
+    # Write the content back to the file with UTF-8 encoding
+    Set-Content -Path $file.FullName -Value $content -Encoding UTF8
+}
+
+Write-Output "Conversion complete."
+
+
